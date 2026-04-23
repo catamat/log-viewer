48701dae2f25be9eb7e27dbaa19987c6093ceba7
gandaldf <gandaldf@users.noreply.github.com>
2026-04-23 13:41:46 +0200

    Added icon and favicon

--- main.go
+++ main.go
@@ -91,7 +91,12 @@ func main() {
 	})
 
 	app.Get("/log-viewer", func(c *fiber.Ctx) error {
-		return c.Type("html").SendString(indexHTML)
+		return c.Type("html").SendString(renderIndexHTML())
+	})
+
+	app.Get("/log-viewer/favicon.svg", func(c *fiber.Ctx) error {
+		c.Set(fiber.HeaderCacheControl, "public, max-age=86400")
+		return c.Type("svg").SendString(appBarIconSVG)
 	})
 
 	app.Get("/log-viewer/api/folders", func(c *fiber.Ctx) error {
@@ -588,12 +593,21 @@ func openBrowser(url string) error {
 	return cmd.Start()
 }
 
-const indexHTML = `<!doctype html>
+func renderIndexHTML() string {
+	return strings.Replace(indexHTMLTemplate, appBarIconPlaceholder, appBarIconSVG, 1)
+}
+
+const appBarIconPlaceholder = "__APPBAR_ICON__"
+
+const appBarIconSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>text-box-search-outline</title><path d="M15.5,12C18,12 20,14 20,16.5C20,17.38 19.75,18.21 19.31,18.9L22.39,22L21,23.39L17.88,20.32C17.19,20.75 16.37,21 15.5,21C13,21 11,19 11,16.5C11,14 13,12 15.5,12M15.5,14A2.5,2.5 0 0,0 13,16.5A2.5,2.5 0 0,0 15.5,19A2.5,2.5 0 0,0 18,16.5A2.5,2.5 0 0,0 15.5,14M5,3H19C20.11,3 21,3.89 21,5V13.03C20.5,12.23 19.81,11.54 19,11V5H5V19H9.5C9.81,19.75 10.26,20.42 10.81,21H5C3.89,21 3,20.11 3,19V5C3,3.89 3.89,3 5,3M7,7H17V9H7V7M7,11H12.03C11.23,11.5 10.54,12.19 10,13H7V11M7,15H9.17C9.06,15.5 9,16 9,16.5V17H7V15Z" /></svg>`
+
+const indexHTMLTemplate = `<!doctype html>
 <html lang="en">
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <title>Log Viewer</title>
+  <link rel="icon" href="/log-viewer/favicon.svg" sizes="any" type="image/svg+xml" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
@@ -626,6 +640,12 @@ const indexHTML = `<!doctype html>
       letter-spacing: 0.04em;
     }
 
+    .appbar-icon {
+      width: 24px;
+      padding-right: 8px;
+      display: flex;
+    }
+      
     .page-content {
       flex: 1;
       min-height: 0;
@@ -762,6 +782,9 @@ const indexHTML = `<!doctype html>
 </head>
 <body>
   <header class="appbar">
+    <div class="appbar-icon">
+      __APPBAR_ICON__
+    </div>
     <div class="appbar-title">Log Viewer</div>
   </header>
 
